0dfbfbed933f7361dadf9a660817c15d5464d925
Phumiphat <phumiphat.skuberg@gmail.com>
2020-10-18 21:46:45 +0700

    localStore state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { loadState, saveState } from "./localStore";
 
+const persistStore = loadState();
 const store = createStore(
   allReducers,
+  persistStore,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
