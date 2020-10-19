68dc1b2d7ecf61a6ecfe6d59a89994ac828b17cc
Phumiphat <phumiphat.skuberg@gmail.com>
2020-10-20 00:54:55 +0700

    detail data

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Button, Container, Row, Col, Image } from "react-bootstrap";
 import { getMethod } from "../../service";
-import './detail.scss';
+import "./detail.scss";
 
 export default class index extends Component {
   state = {
@@ -17,7 +17,6 @@ export default class index extends Component {
     getMethod(url).then((response) => {
       const data = response.data;
       this.setState({ data });
-      console.log(this.state.data);
     });
   }
 
@@ -27,12 +26,23 @@ export default class index extends Component {
         <Container>
           <Row>
             <Col xs={4}>
-              <Image src={this.state.data.image_url} rounded width="300"/>
+              <Image src={this.state.data.image_url} rounded width="300" />
             </Col>
             <Col sx={8}>
               <h3>{this.state.data.name}</h3>
               <p>{this.state.data.description}</p>
               <div className="mt-5 text-price">฿{this.state.data.price}</div>
+              <div className="mt-3 add-cart">
+                <div className="d-flex justify-content-start align-items-center">
+                  <div>
+                    <Button variant="primary">-</Button>
+                  </div>
+                  <div className="num-add">1</div>
+                  <div>
+                    <Button variant="primary">+</Button>
+                  </div>
+                </div>
+              </div>
               <Button variant="secondary" className="mt-3">
                 Add to Cart
               </Button>
