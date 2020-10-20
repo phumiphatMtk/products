ed6b50408a3d11eab30ae3ed085ac9b40d5a0b9d
Phumiphat <phumiphat.skuberg@gmail.com>
2020-10-20 10:32:27 +0700

    increment decrement

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -17,34 +17,53 @@ export default class index extends Component {
     const url = "https://cc-mock-api.herokuapp.com/product/" + id;
     getMethod(url).then((response) => {
       const data = response.data;
+      data.num = 1;
       this.setState({ data });
     });
   }
 
-  // gotoHome() {
-  //   window.location.href = "/";
-  // }
+  increment = () => {
+    const data = this.state.data;
+    data.num = data.num + 1;
+    this.setState({ data });
+  };
+
+  decrement = () => {
+    const data = this.state.data;
+    if (data.num >= 1) {
+      data.num = data.num - 1;
+      this.setState({ data });
+    } else {
+      data.num = data.num;
+      this.setState({ data });
+    }
+  };
 
   render() {
+    const props = this.state.data;
     return (
       <div className="my-5 detail">
         <Container>
           <Row>
             <Col xs={4}>
-              <Image src={this.state.data.image_url} rounded width="300" />
+              <Image src={props.image_url} rounded width="300" />
             </Col>
             <Col sx={8}>
-              <h3>{this.state.data.name}</h3>
-              <p>{this.state.data.description}</p>
-              <div className="mt-5 text-price">฿{this.state.data.price}</div>
+              <h3>{props.name}</h3>
+              <p>{props.description}</p>
+              <div className="mt-5 text-price">฿{props.price}</div>
               <div className="mt-3 add-cart">
                 <div className="d-flex justify-content-start align-items-center">
                   <div>
-                    <Button variant="primary">-</Button>
+                    <Button variant="primary" onClick={this.decrement}>
+                      -
+                    </Button>
                   </div>
-                  <div className="num-add">1</div>
+                  <div className="num-add">{props.num}</div>
                   <div>
-                    <Button variant="primary">+</Button>
+                    <Button variant="primary" onClick={this.increment}>
+                      +
+                    </Button>
                   </div>
                 </div>
               </div>
@@ -53,9 +72,7 @@ export default class index extends Component {
                   Add to Cart
                 </Button>
                 <Link to="/">
-                  <Button variant="primary">
-                    Back to product list
-                  </Button>
+                  <Button variant="primary">Back to product list</Button>
                 </Link>
               </div>
             </Col>
